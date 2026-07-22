93da4e9a88b54ce21e035936c6ef96350f607074
JDaver <gundogdudaver@gmail.com>
2026-07-22 22:24:31 +0200

    Elevation Profile Improvement

--- Scripts/Generation/WorldGeneration/WorldArchetype/Rules/ElevationProfile/ElevationProfile.cs
+++ Scripts/Generation/WorldGeneration/WorldArchetype/Rules/ElevationProfile/ElevationProfile.cs
@@ -3,5 +3,11 @@ public class ElevationProfile
 {
     public string Type { get; set; }
 
+    public string Function { get; set; }
+
+    public int Min { get; set; }
+
+    public int Max {get; set; }
+    
     public float Weight { get; set; }
 }
